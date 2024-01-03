d1976266d3b00094f067abe34b41f6112c8041e9
Muhammad Zidan Ramdhani <86590725+EddieCrispo@users.noreply.github.com>
2024-01-03 15:53:57 +0700

    Change what model to use
    Change model to "knn_oversampling+tuning_model.pkl"

--- hungarian.py
+++ hungarian.py
@@ -87,7 +87,7 @@ smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
 # model = pickle.load(open("model/xgb_model.pkl", 'rb'))
-model = pickle.load(open("model/best_knn_model.pkl", 'rb'))
+model = pickle.load(open("model/knn_oversampling+tuning_model.pkl", 'rb'))
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
