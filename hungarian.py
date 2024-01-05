50caea48f583a899d978988ccfceed43db24dcc7
Muhammad Zidan Ramdhani <86590725+EddieCrispo@users.noreply.github.com>
2024-01-05 07:39:22 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -90,9 +90,10 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
-model = pickle.load(open("model/xgb_model.pkl", 'rb'))
-# picklemodel = pickle.load(open("model/knn_ovtuning_model.pkl", 'rb'))
-# model = joblib.load("model/knn_ovtuning_model.joblib")
+model = joblib.load("model/knn_ovtuning_model.joblib")
+# model = pickle.load(open("model/xgb_model.pkl", 'rb'))
+picklemodel = pickle.load(open("model/knn_ovtuning_model.pkl", 'rb'))
+
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
