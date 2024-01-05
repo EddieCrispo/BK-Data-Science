8671200b219e67c149916444a2767ef8140d0487
Muhammad Zidan Ramdhani <86590725+EddieCrispo@users.noreply.github.com>
2024-01-05 07:05:46 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -91,9 +91,7 @@ X, y = smote.fit_resample(X, y)
 
 # model = pickle.load(open("model/xgb_model.pkl", 'rb'))
 # model = pickle.load(open("model/xgb_ov_tuning_model.pkl", 'rb'))
-# model = pickle.load(open("model/knn_ovtuning_model.pkl", 'rb'))
-with open("model/knn_ovtuning_model.pkl", 'wb') as model_file:
-    pickle.dump(knn_ovtuning_model, model_file, protocol=pickle.HIGHEST_PROTOCOL)
+model = pickle.load(open("model/knn_ovtuning_model.pkl", 'rb'))
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
