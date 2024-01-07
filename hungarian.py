f74bd7720e65e53bf2a751c1a0a553e50cc91857
Muhammad Zidan Ramdhani <86590725+EddieCrispo@users.noreply.github.com>
2024-01-07 09:20:53 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -1,7 +1,6 @@
 import itertools
 import pandas as pd
 import numpy as np
-import xgboost
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
